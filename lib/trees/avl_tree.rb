d66a8a8015d41976073c661c34b0799f1ec59c2c
lostintuition <theanonymous@gmail.com>
2015-09-30 09:39:45 -0700

    Finished the AVL tree implementation

--- lib/trees/avl_tree.rb
+++ lib/trees/avl_tree.rb
@@ -8,23 +8,29 @@ class AvlTree
   end
 
   def insert(key, value)
-    insert_node(@root, key, value)
+    @root = insert_node(@root, key, value)
   end
 
   def insert_node(node, key, value)
     return Node.new(key, value) if node.nil?
 
     if key < node.key
-      insert_node(node.left, key, value)
+      node.left = insert_node(node.left, key, value)
     elsif key > node.key
-      insert_node(node.right, key, value)
+      node.right = insert_node(node.right, key, value)
     end
+
+    update_height(node)
 
     balance = get_balance(node, key)
     if balance == Balance::LEFT_RIGHT
+      node.left = left_rotate(node.left)
+      return right_rotate(node)
     elsif balance == Balance::LEFT_LEFT
       return right_rotate(node)
     elsif balance == Balance::RIGHT_LEFT
+      node.right = right_rotate(node.right)
+      return left_rotate(node)
     elsif balance == Balance::RIGHT_RIGHT
       return left_rotate(node)
     end
@@ -32,23 +38,153 @@ class AvlTree
     return node
   end
 
+  def getMinNode(node)
+    current = node
+    while (!current.left.nil?)
+      current = current.left
+    end
+
+    return current
+  end
+
+  def delete(key)
+    @root = delete_node(@root, key)
+  end
+
+  def delete_node(node, key)
+    return nil if node.nil?
+
+    if key < node.key
+      node.left = delete_node(node.left, key)
+    elsif key > node.key
+      node.right = delete_node(node.right, key)
+    else
+      if node.left.nil? || node.right.nil?
+        node = node.left.nil? ? node.right : node.left
+      else
+        min_node = getMinNode(node.right)
+        node.key = min_node.key
+        node.value = min_node.value
+        node.right = delete_node(node.right, min_node.key)
+      end
+    end
+
+    return nil if node.nil?
+
+    update_height(node)
+    balance = get_delete_balance(node, key)
+
+    if balance == Balance::LEFT_LEFT
+      return right_rotate(node)
+    elsif balance == Balance::LEFT_RIGHT
+      node.left = left_rotate(node.left)
+      return right_rotate(node)
+    elsif balance == Balance::RIGHT_RIGHT
+      return left_rotate(node)
+    elsif balance == Balance::RIGHT_LEFT
+      node.right = right_rotate(node.right)
+      return left_rotate(node)
+    end
+
+    return node
+  end
+
+  def right_rotate(node)
+    new_right_child = node
+    new_root = node.left
+
+    new_right_child.left = new_root.right
+    new_root.right = new_right_child
+
+    update_height(new_right_child)
+    update_height(new_root)
+    return new_root
+  end
+
+  def left_rotate(node)
+    new_root = node.right
+    new_left_child = node
+
+    new_left_child.right = new_root.left
+    new_root.left = new_left_child
+
+    update_height(new_left_child)
+    update_height(new_root)
+    return new_root
+  end
+
+  def update_height(node)
+    node.height = [get_height(node.left), get_height(node.right)].max + 1
+  end
+
+  # compares key to the left and right keys because that determines the path from the newly inserted leaf
   def get_balance(node, key)
     height_difference = get_height(node.left) - get_height(node.right)
-    if height_difference > 0 && key < node.left.key
+    if height_difference > 1 && key < node.left.key
       return Balance::LEFT_LEFT
-    elsif height_difference > 0 && key > node.left.key
+    elsif height_difference > 1 && key > node.left.key
       return Balance::LEFT_RIGHT
-    elsif height_difference < 0 && key < node.right.key
+    elsif height_difference < -1 && key < node.right.key
       return Balance::RIGHT_LEFT
-    elsif height_difference < 0 && key > node.right.key
+    elsif height_difference < -1 && key > node.right.key
       return Balance::RIGHT_RIGHT
     else
       return Balance::BALANCED
     end
   end
 
+  def get_delete_balance(node, key)
+    height_difference = get_height_difference(node)
+    if height_difference > 1 && get_height_difference(node.left) > 1
+      return Balance::LEFT_LEFT
+    elsif height_difference > 1 && get_height_difference(node.left) < -1
+      return Balance::LEFT_RIGHT
+    elsif height_difference < -1 && get_height_difference(node.right) > 1
+      return Balance::RIGHT_LEFT
+    elsif height_difference < -1 && get_height_difference(node.right) < -1
+      return Balance::RIGHT_RIGHT
+    else
+      return Balance::BALANCED
+    end
+  end
+
+  def get_height_difference(node)
+    return 0 if node.nil?
+    return get_height(node.left) - get_height(node.right)
+  end
+
   def get_height(node)
     return node.nil? ? -1 : node.height
+  end
+
+  def preorder
+    accumulator = []
+    preorder_helper(@root, accumulator)
+    return accumulator
+  end
+
+  def preorder_helper(node, accumulator)
+    return if node.nil?
+
+    preorder_helper(node.left, accumulator)
+    accumulator << node.value
+    preorder_helper(node.right, accumulator)
+  end
+
+  def get(key)
+    return get_helper(@root, key)
+  end
+
+  def get_helper(node, key)
+    return nil if node.nil?
+
+    if key < node.key
+      return get_helper(node.left, key)
+    elsif key > node.key
+      return get_helper(node.right, key)
+    else
+      return node.value
+    end
   end
 
   class Node
--- spec/trees/avl_tree_spec.rb
+++ spec/trees/avl_tree_spec.rb
@@ -4,26 +4,107 @@ describe AvlTree do
 
   describe "#put" do
     context "when items are put in" do
-      subject(:red_black_tree) { RedBlackTree.new }
-      let(:key_to_value) { { 0 => 15, 1 => 27, 2 => 14, 4 => 32 } }
+      subject(:avl_tree) { AvlTree.new }
+      let(:key_to_value) do
+        key_to_value = {}
+        100.times.map do
+          random_value = Random.rand(1000)
+          key_to_value[random_value] = random_value
+        end
+        key_to_value
+      end
+
       before do
         key_to_value.each do |key, value|
-          red_black_tree.put(key, value)
+          avl_tree.insert(key, value)
         end
       end
 
       it "has a sorted preorder" do
-        avl_tree = AvlTree.new
         sorted_keys = key_to_value.keys.sort
         preorder = sorted_keys.collect { |key| key_to_value[key] }
-        expect(red_black_tree.preorder).to contain_exactly(*preorder)
+        expect(avl_tree.preorder).to contain_exactly(*preorder)
       end
 
       it "contains the value for each key" do
         key_to_value.keys.each do |key|
-          expect(red_black_tree.get(key)).to eq(key_to_value[key])
+          expect(avl_tree.get(key)).to eq(key_to_value[key])
         end
       end
     end
+  end
+
+  describe "#delete" do
+    context "when items are put in" do
+      subject(:avl_tree) { AvlTree.new }
+      let(:key_to_value) do
+        key_to_value = {}
+        1000.times.map do
+          random_value = Random.rand(100000)
+          key_to_value[random_value] = random_value
+        end
+        key_to_value
+      end
+
+      before do
+        key_to_value.each do |key, value|
+          avl_tree.insert(key, value)
+        end
+      end
+
+      it "has a sorted preorder" do
+        keys = key_to_value.keys
+        250.times.map do
+          random_index = Random.new.rand(0..keys.size - 1)
+          key = keys.delete_at(random_index)
+          avl_tree.delete(key)
+        end
+
+        sorted_keys = keys.sort
+        preorder = sorted_keys
+        puts avl_tree.preorder.size
+        puts key_to_value.keys.size
+        expect(avl_tree.preorder).to contain_exactly(*preorder)
+      end
+
+      it "contains the value for each key" do
+        key_to_value.keys.each do |key|
+          expect(avl_tree.get(key)).to eq(key_to_value[key])
+        end
+      end
+    end
+  end
+
+
+  describe "#rotate_right" do
+    subject(:avl_tree) { AvlTree.new }
+
+    it "maintains a sorted preorder" do
+      root = AvlTree::Node.new(4, 4)
+      root.left = AvlTree::Node.new(2, 2)
+      root.left.left = AvlTree::Node.new(1, 1)
+      root.left.right = AvlTree::Node.new(3, 3)
+      root.right = AvlTree::Node.new(5, 5)
+
+      original_preorder = preorder(root)
+      rotated_preorder = preorder(avl_tree.right_rotate(root))
+
+      expect(rotated_preorder).to contain_exactly(*original_preorder)
+    end
+  end
+
+  def preorder(node)
+    accumulator = []
+    preorder_helper(node, accumulator)
+    return accumulator
+  end
+
+  def preorder_helper(node, accumulator)
+    return if node.nil?
+
+    preorder_helper(node.left, accumulator)
+    accumulator << node.value
+    preorder_helper(node.right, accumulator)
+
   end
 end
